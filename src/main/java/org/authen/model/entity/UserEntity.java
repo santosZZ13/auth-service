9aecf70e6f3e42434ad54d3824326f43604796dd
NamQuang13 <dragonsang10a1@gmail.com>
2024-04-14 23:06:24 +0700

    init

--- src/main/java/org/authen/model/entity/UserEntity.java
+++ src/main/java/org/authen/model/entity/UserEntity.java
@@ -5,13 +5,14 @@ import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.io.Serializable;
 import java.util.Date;
 
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "user")
-public class UserEntity extends UserBaseEntity {
+public class UserEntity extends UserBaseEntity implements Serializable {
 	@Column(name = "created_at")
 	private Date createdAt;
 	@Column(name = "updated_at")
