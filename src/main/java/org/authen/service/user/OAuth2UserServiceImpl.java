b597f950db4b23fc219e7691f4c9491ed1fa64cc
nam.dinh <dragonsang10a1@gmail.com>
2024-05-14 16:49:40 +0700

    init

--- src/main/java/org/authen/service/user/OAuth2UserServiceImpl.java
+++ src/main/java/org/authen/service/user/OAuth2UserServiceImpl.java
@@ -8,17 +8,27 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
 
+	/**
+	 * if add openId
+	 * @param oAuth2UserRequest the user request
+	 * @return
+	 * @throws OAuth2AuthenticationException
+	 */
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
-		Map<String, Object> attributes = oAuth2User.getAttributes();
+
+		// Check if the user exists in your database.
+		// If not, create a new user.
+		// Return the OAuth2User object.
+
+		// Customize the OAuth2User object and return the customized object.
 		return oAuth2User;
 	}
 }
